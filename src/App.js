97c6323c0dd5f1ba5c2f94c7a68f1f991739f7fe
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2020-12-16 12:43:59 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Travis-ugo</h1>
+      <h2>First React Application</h2>
+      <p>Hey home Boy. Deploy to AWS Asap</p>
+    </div>
+  );
+}
